85620e21ced21b8662c1faae9677cecadd4c3a84
Cristianzxcd <cristianlopezpalacios148@gmail.com>
2024-04-28 17:33:09 -0600

    cambio a la nueva base

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -15,11 +15,11 @@ namespace tienda.Models
     public class Usuario : Conexion
     {
         public int IdUsuario { get; set; }
-        public int Rol { get; set; }
+        public int Id_rol { get; set; }
         public string NombreUsuario { get; set; }
         public string Correo { get; set; }
-        public string Contraseña { get; set; }
-        public int Estatus { get; set; }
+        public string Pass { get; set; }
+        public string Estado { get; set; }
         public string Foto { get; set; }
 
         public Usuario()
@@ -28,24 +28,24 @@ namespace tienda.Models
 
 
         // Constructor con parámetros
-        public Usuario(int idUsuario, int rol, string nombreUsuario, string correo, string contraseña, int estatus, string foto)
+        public Usuario(int idUsuario, int id_rol, string nombreUsuario, string correo, string pass, string estado, string foto)
         {
             IdUsuario = idUsuario;
-            Rol = rol;
+            Id_rol = id_rol;
             NombreUsuario = nombreUsuario;
             Correo = correo;
-            Contraseña = contraseña;
-            Estatus = estatus;
+            Pass = pass;
+            Estado = estado;
             Foto = foto;
         }
         public void SetFromDataRow(DataRow row)
         {
             IdUsuario = Convert.ToInt32(row["id_usuario"]);
-            Rol = Convert.ToInt32(row["rol"]);
+            Id_rol = Convert.ToInt32(row["id_rol"]);
             NombreUsuario = row["nombre_usuario"].ToString();
             Correo = row["correo"].ToString();
-            Contraseña = row["contrasena"].ToString();
-            Estatus = Convert.ToInt32(row["estatus"]);
+            Pass = row["pass"].ToString();
+            Estado = row["estado"].ToString();
         }
 
         public bool GuardarUsuario(Usuario usuario)
@@ -55,15 +55,15 @@ namespace tienda.Models
                 return false;
             }
             NpgsqlParameter correo = new NpgsqlParameter("correo", usuario.Correo);
-            NpgsqlParameter contraseña = new NpgsqlParameter("contrasena", usuario.Contraseña);
-            NpgsqlParameter estatus = new NpgsqlParameter("estatus", usuario.Estatus = 1);
+            NpgsqlParameter pass = new NpgsqlParameter("pass", usuario.Pass);
+            NpgsqlParameter estado = new NpgsqlParameter("estado", usuario.Estado = "activo");
             NpgsqlParameter nombreUsuario = new NpgsqlParameter("nombreUsuario", usuario.NombreUsuario);
-            NpgsqlParameter rol = new NpgsqlParameter("rol", usuario.Rol = 1);
+            NpgsqlParameter id_rol = new NpgsqlParameter("id_rol", usuario.Id_rol = 1);
             List<NpgsqlParameter> lts = new List<NpgsqlParameter>
             {
-                correo,contraseña,estatus,nombreUsuario,rol
+                correo,pass,estado,nombreUsuario,id_rol
             };
-            const string sql = "INSERT INTO usuario (rol,nombre_usuario,correo,contrasena,estatus) VALUES (@rol,@nombreUsuario,@correo,@contrasena,@estatus)";
+            const string sql = "INSERT INTO usuario (id_rol,nombre_usuario,correo,pass,estado) VALUES (@id_rol,@nombreUsuario,@correo,@pass,@estado)";
             int filasAfectadas = ExecuteQuery(sql, lts);
             if (filasAfectadas > 0)
             {
@@ -79,12 +79,12 @@ namespace tienda.Models
                return false;
             }
             NpgsqlParameter correo = new NpgsqlParameter("correo", usuario.Correo);
-            NpgsqlParameter contrasena = new NpgsqlParameter("contrasena", usuario.Contraseña);
+            NpgsqlParameter pass = new NpgsqlParameter("pass", usuario.Pass);
             List<NpgsqlParameter> lts = new List<NpgsqlParameter>
             {
-                correo, contrasena
+                correo, pass
             };
-            const string sql = "Select * From usuario where correo = @correo and contrasena = @contrasena and estatus = 1";
+            const string sql = "Select * From usuario where correo = @correo and pass = @pass and estado = 'activo'";
             DataTable dt = GetQuery(sql, lts);
             if (dt != null && dt.Rows.Count > 0)
             {
