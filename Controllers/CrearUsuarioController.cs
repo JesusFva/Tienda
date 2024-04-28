2877e60a6958b277c096b873e0f766eb40ea2f97
Cristianzxcd <cristianlopezpalacios148@gmail.com>
2024-04-27 21:02:33 -0600

    Vista Usuario

---
+++ Controllers/CrearUsuarioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace tienda.Controllers
+{
+    public class CrearUsuarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
