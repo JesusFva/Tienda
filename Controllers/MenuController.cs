5255186bd1bc966c840d53f7f3fa2765057569da
Cristianzxcd <cristianlopezpalacios148@gmail.com>
2024-04-28 22:33:47 -0600

    añadiendo la vista menu

---
+++ Controllers/MenuController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using tienda.Models;
+
+namespace tienda.Controllers
+{
+    public class MenuController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    
+
+
+    }
+}
